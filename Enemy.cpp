f89ae05621bdc7d04dd53233dc74d53954b72a6b
Zentyre <dragonslayervc@gmail.com>
2023-12-19 10:24:23 -0500

    fixed little bug

--- Enemy.cpp
+++ Enemy.cpp
@@ -37,6 +37,49 @@ void Enemy::attack(Character* Target) {
             Target->takeDmg(y);
         }
     }
+     if (trickstermovebool == true && health > 0) {
+        int attackself, y;
+        critchance = r() % 100 + 1;
+        attackself = r() % 100 + 1;
+        if (attackself > 60 && critchance > 70) {
+             y = r() % 7 + attackPower * 1.5;
+             health -= y;
+             cout << "The enemy is confused and attacked itself for" << y << " damage!" << endl;
+        }
+        else if (attackself > 60) {
+             y = r() % 7 + attackPower;
+             health -= y;
+             cout << "The enemy is confused and attacked itself for" << y << " damage!" << endl;
+        }
+        else if (attackself <= 60 && critchance > 70) {
+            cout << "The enemy resisted your willpower and did not get confused." << endl;
+            y = r() % 7 + attackPower * 1.5;
+            Target->takeDmg(y);
+        }
+        else {
+            cout << "The enemy resisted your willpower and did not get confused." << endl;
+            y = r() % 7 + attackPower;
+            Target->takeDmg(y);
+        }
+    }
+    else if (health > 0 && getlevel() >= 100) {
+        int y, accuracy;
+        accuracy = r() % 100 + 1;
+        critchance = r() % 100 + 1;
+        if (accuracy > 10 && critchance <= 5) {
+            y = r() % 10 + attackPower * 1.5;
+            cout << name << " dealt " << y << " damage." << endl;
+            Target->takeDmg(y);
+        }
+        else if (accuracy > 10) {
+            y = r() % 10 + attackPower;
+            cout << name << " dealt " << y << " damage." << endl;
+            Target->takeDmg(y);
+        }
+        else if (accuracy <= 10) {
+            cout << name << " has missed its attack!" << endl;
+        }
+    }
     else if (health > 0) {
         int y, accuracy;
         accuracy = r() % 100 + 1;
