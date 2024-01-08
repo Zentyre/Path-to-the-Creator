c1fdd3eb2c31d595aa4a591a0e9016c2f4a4dce1
Zentyre <dragonslayervc@gmail.com>
2024-01-08 15:04:30 -0500

    enemies have defence now

--- Enemy.h
+++ Enemy.h
@@ -8,6 +8,16 @@ public:
 	Enemy(string n2, int hp2, int atk2, int def2, int live2, int lvl2, int maxh2);
 	void attack(Character* Target) override;
 	void takeDmg(int dmg) override {
+		if (defence > dmg) {
+			cout << "Your defence absorbed all " << dmg << " enemy damage!" << endl;
+		}
+		else if (defence > 0 && defence < dmg) {
+			cout << "Your defence absorbed " << getdefence() << " damage!" << endl;
+		}
+		dmg -= defence;
+		if (dmg < 0) {
+			dmg = 0;
+		}
 		health -= dmg;
 		if (health <= 0) {
 			lives -= 1;
